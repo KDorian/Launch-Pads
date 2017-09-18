45f863f76bcd0196fffd59d5592018287507d88f
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-09-18 19:43:26 +0200

    Add filtering

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ $(document).ready(function() {
       dataType: "json",
       success: function(response) {
         $.each(response.apartments, function(i, apartment) {
-          var listing = "<a href='#' class='list-group-item'><h4 class='list-group-item-heading'>" + apartment.description + " / " + apartment.bedrooms + " / " + apartment.price + "</h4><p class='list-group-item-text'> " + apartment.neighborhood + "</p></a>";
+          var apartmentClass = apartment.city.toLowerCase().replace(" ", "-");
+          var listing = "<a href='#' class='list-group-item " + apartmentClass + " listings'><h4 class='list-group-item-heading'>" + apartment.description + " / " + apartment.bedrooms + " BR / " + apartment.price + "</h4><p class='list-group-item-text'> " + apartment.neighborhood + "</p></a>";
           $(".apartments").append(listing);
         });
+      },
+      error: function(error) {
+        console.log(error);
       }
     });
+
+    $(".filter").click(function() {
+      $(".filter").removeClass("active");
+      $(this).addClass("active");
+
+      $(".listings").show();
+
+      var city = $(this).attr("id");
+      if (city !== "all") {
+        $(".listings").not("." + city).css("display", "none");
+      }
+
+    })
 });
