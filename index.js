244db8b5ff3e7e93037dcca1eb8bdec1e5b05abc
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-09-18 19:57:12 +0200

    Add the google mpas to listings

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
       success: function(response) {
         $.each(response.apartments, function(i, apartment) {
           var apartmentClass = apartment.city.toLowerCase().replace(" ", "-");
-          var listing = "<a href='#' class='list-group-item " + apartmentClass + " listings'><h4 class='list-group-item-heading'>" + apartment.description + " / " + apartment.bedrooms + " BR / " + apartment.price + "</h4><p class='list-group-item-text'> " + apartment.neighborhood + "</p></a>";
+          var listing = "<a href='#' id=" + apartment.id + " class='list-group-item " + apartmentClass + " listings'><h4 class='list-group-item-heading'>" + apartment.description + " / " + apartment.bedrooms + " BR / " + apartment.price + "</h4><p class='list-group-item-text'> " + apartment.neighborhood + "</p></a>";
           $(".apartments").append(listing);
         });
       },
@@ -25,5 +25,23 @@ $(document).ready(function() {
         $(".listings").not("." + city).css("display", "none");
       }
 
-    })
+    });
+
+    $(document).on("click", ".listings", function() {
+      var id = $(this).attr("id");
+      $.ajax({
+        url: "https://api.myjson.com/bins/2sadq?pretty=1",
+        dataType: "json",
+        success: function(response) {
+          var selectedApartment = $.grep(response.apartments, function(apartment){
+            return apartment.id == id;
+          })
+          var address = selectedApartment[0].address;
+          window.open("http://maps.google.com/?q=" + address);
+        },
+        error: function(error) {
+          console.log(error);
+        }
+      });
+    });
 });
